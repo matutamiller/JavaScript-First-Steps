25065f4a0e1f7fdfca5da0e236e8e0bc16dc37cb
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-04 17:59:09 +0100

    Variables - Objects

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,8 +1,4 @@
-var var1 = [1, 2, 3, '4'];
-console.log(var1[0]);
-console.log(var1[4]);
-var var2 = null;
-console.log(var2);
-console.log(typeof var2);
-var var3 = undefined;
-console.log(typeof var3);
+var var1 = {
+    name: 'Matie'
+};
+console.log( typeof var1);
