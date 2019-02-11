91c5bda18c222688f369a3f1e649972121a95ce4
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-11 19:01:44 +0100

    Control Structures - For Loop

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,10 +1,3 @@
-var luckyNumber = 8;
-
-switch (luckyNumber) {
-    case 1: 
-        console.log('Is 1');   
-    case 8:
-        console.log('Is 8');
-    default:
-        console.log('Default');            
+for (var i = 0; i < 5; i++) {
+    console.log(i);
 }
