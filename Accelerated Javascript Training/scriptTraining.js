af26d00f7f15950a0fcfe77c0a92aead1c50cfbc
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-11 18:58:05 +0100

    Control Structures - Switch & Break

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -2,12 +2,9 @@ var luckyNumber = 8;
 
 switch (luckyNumber) {
     case 1: 
-        console.log('Is 1');
-        break;
+        console.log('Is 1');   
     case 8:
         console.log('Is 8');
-        break; 
     default:
-        console.log('Default'); 
-        break;    
+        console.log('Default');            
 }
