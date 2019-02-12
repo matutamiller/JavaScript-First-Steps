fab9c6a2ab4f2722723b8704c80a1d2ecb3e6fa3
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-12 20:14:33 +0100

    Controlling Loop with Break and Continue

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,4 +1,9 @@
 for (var i = 0; i < 5; i++) {
-    for (j = 0; j < 2; j++)
-    console.log(i * j);
+    for (var j = 0; j < 2; j++) {
+        if(i == 1) {
+            break;
+        }
+        console.log('Inside inner loop, j = ' + j)
+    }    
+    console.log(i);
 }
