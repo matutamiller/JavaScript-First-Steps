1d3f4938bce7644f32bfe37ee88ab077b3795d73
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-16 17:00:09 +0100

    Multiplication & Floating Point Problems

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,9 +1,21 @@
-var a = "Split us";
-var b = "us";
+var a = 12;
+var b = 2;
 
-console.log(a - b);
+a *= b;
+console.log(a);
 
-var c = 12;
-var d = '1';
+var c = 1.3;
+var d = 2.2;
 
-console.log(c - d);
+console.log(c * d);
+
+if ((c * d).toFixed(2) == 2.86) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+var e = 2;
+var f = null;
+
+console.log(e * f);
