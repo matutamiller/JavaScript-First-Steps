9ff8b0ba7e68764a60b7329cc40c6fbcdf6dbc6f
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-20 17:53:54 +0100

    Operators - Comparing Values

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,15 +1,5 @@
-var a = 3.3;
-var b = 2.2;
-console.log((a / b).toFixed(2));
-
-var a = 10;
-var b = 3;
-console.log(a % b);
-
-var a = 10;
-var b = 0;
-console.log(a / b);
-
-var a = 10;
-var b = Infinity;
-console.log(a / b);
+console.log(1 === 1);
+console.log(1 !== '1');
+console.log(1 == '1');
+console.log(1 >= 1);
+console.log(1 >= '1');
