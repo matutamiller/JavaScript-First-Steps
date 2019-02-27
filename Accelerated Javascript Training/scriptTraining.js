a7e3b7df089785b7f2f361abb72faa59f57a85a3
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-02-27 17:06:42 +0100

    Global vs. Local Scope

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,20 +1,8 @@
-var aNumber = 5;
-console.log(aNumber);
-var anotherNumber = aNumber;
-console.log(anotherNumber);
-aNumber = 12;
-console.log(aNumber);
-console.log(anotherNumber);
+var test = 'Global scope';
 
-var array = [1,2,3];
-var anotherArray = array;
-console.log(array);
-console.log(anotherArray);
 
-array.push(4);
-console.log(array);
-console.log(anotherArray);
+function localScope() {
+    console.log(test);
+}
 
-array = ['a', 'b'];
-console.log(array);
-console.log(anotherArray);
+localScope();
