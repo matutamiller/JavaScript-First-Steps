ffe96ab268fb437786afb46a48b23db630b006f3
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-03-09 18:12:52 +0100

    The Difference Between Concat and Join

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,23 +1,11 @@
-var array = [1, 2, 3, 4];
+var array = ['old', 1, 2, 3, 4];
+var newArray = ['join', 'me'];
 
-array.unshift('new');
-array[array.indexOf('new')] = 'old';
-console.log(array.filter(function(value){
-    return value > 2;
-}));
-
-var array = [1, 2, 3, 4];
-
-array.unshift('new');
-array[array.indexOf('new')] = 'old';
-console.log(array.map(function(value){
-    return value * 2;
-}));
+console.log(array.concat(newArray));
 console.log(array);
 
-var array = [1, 2, 3, 4];
+var array = ['old', 1, 2, 3, 4];
+var newArray = ['join', 'me'];
 
-array.unshift('new');
-array[array.indexOf('new')] = 'old';
-console.log(array.reverse());
+console.log(array.join(''));
 console.log(array);
