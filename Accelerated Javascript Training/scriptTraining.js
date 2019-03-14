5238e29023f991fb76c7f3d2e1945a659c4580e5
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-03-14 18:49:51 +0100

    Properties & "this"

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -6,24 +6,7 @@ var person = {
         location: 'Poland'
     },
     greet: function() {
-        console.group('Hello!');
+        console.group('Hello!, I am ' + this.age + ' years old');
     }
 };
 person.greet();
-console.log(person.details.hobbies);
-console.log(typeof person);
-console.log(typeof person.name);
-
-var person = {
-    name: 'Matie',
-    age: 33,
-    details: {
-        hobbies: ["Sports", "Cooking"],
-        location: 'Poland'
-    },
-    greet: function() {
-        console.group('Hello!');
-    }
-};
-
-console.log(typeof person['first-name']);
