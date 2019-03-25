fcba25487b5b8aa4be72ed5b35b5178c112cabed
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-03-25 18:59:34 +0100

    Constructor Functions

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,14 +1,8 @@
-var person = {
-    name:'Matie',
-    age: 33,
-};
+function Person() {
 
-Object.prototype.greet = function(){
-    console.log('Hello there, I am ' + this.name + '!');
-};
+}
 
-person.greet();
-var max = Object.create(person);
-var anna = Object.create(person);
-console.log(anna.__proto__.__proto__ == Object.prototype);
-console.log(Object.getPrototypeOf(anna) == person);
+var person = new Person();
+person.name = "Matie";
+
+console.log(person.__proto__ == Person.prototype);
