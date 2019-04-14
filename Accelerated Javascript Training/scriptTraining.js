779f73957d81d30b8d83a489f24c4b4efe859c4b
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-04-14 15:58:06 +0200

    Important built-in Methods and Properties

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,27 +1,11 @@
-var account = {
-    cash: 12000,
-    _name: 'Default',
-    withdraw: function(amount) {
-        this.cash -=amount;
-        console.log('withdrew ' + amount + ', new cash reserve is: ' + this.cash);
-    }
+var person = {
+   name: 'Matie',
+   age: 33,
+   greet: function() {
+       console.log('Hello');
+   }
 };
 
-
-
-Object.defineProperty(account, 'name',{
-    get: function() {
-        this._name;
-    },
-    set: function(name) {
-        if (name == 'Matie'){
-            this._name = name;
-        } 
-    }
-});
-
-console.log(account.name);
-
-account.name = 'ID000-3';
-
-console.log(account.name);
+for (var field in person) {
+    console.log(field);
+}
