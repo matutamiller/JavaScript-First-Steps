14ea8c5225d4492f898899be87d79d31d4702a49
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-04-18 18:52:55 +0200

    Handling Errors with try and catch

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,18 +1,7 @@
-var a = 2;
-var b = 5;
-var c;
-var d = 1000;
-
-for (var i = 0; i < 5; i++) {
-    if (i > a && i < b) {
-        c = 1000;
-    }
-    if (i === 3) {
-        d *= 2;
-    }
+try {
+    test();
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Finally!');
 }
-
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
