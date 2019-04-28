c726886ecae0210d2ffffe5f7b35d16e1cf4840b
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-04-28 19:49:12 +0200

    Timers and Intervals

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,21 +1,3 @@
-function message(message){
-    console.log(message);
-    console.log(arguments);
-}
-
-message('Hi!', 10);
-
-function message(message){
-    console.log(message);
-    console.log(arguments.length);
-}
-
-message('Hi!', 10);
-var msg = message;
-console.log(msg.name);
-console.log(msg.length);
-
-var msg = function(message) {
-    console.log(message);
-}
-console.log(msg.name);
+setTimeout(function(){
+    console.log('Finished!');
+}, 2000);
