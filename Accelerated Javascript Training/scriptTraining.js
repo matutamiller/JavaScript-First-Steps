6ada16d0d52cec8df9c0e261cbfe1a710ea359f0
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-05-14 20:21:11 +0200

    The Date Object

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,6 +1,2 @@
-console.log(Math.max(1, 100, 1000));
-console.log(Math.min(1, 100, 1000));
-console.log(Math.random());
-
-var rnd = Math.floor(Math.random() * 100) + 1;
-console.log(rnd);
+var today = new Date();
+console.log(today);
