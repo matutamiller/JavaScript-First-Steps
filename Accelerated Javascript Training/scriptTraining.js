363216c68262c4aadfcdd01aa106316533d53721
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-05-14 20:27:00 +0200

    The Date Object

--- Accelerated Javascript Training/scriptTraining.js
+++ Accelerated Javascript Training/scriptTraining.js
@@ -1,5 +1,3 @@
 var today = new Date();
-console.log(today.toString());
-
-var today = new Date(2016, 5, 23);
-console.log(today.toString());
+console.log(today.getDate());
+console.log(today.getDay());
